add128a5a2cc2fb6911c5dee2af176d91eea7afa
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-07 11:21:58 +0500

    📝 Added /start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,20 +4,19 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp, db, bot
 from data.config import ADMINS
+from utils.define_is_member import is_member
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    try:
-        user = await db.add_user(telegram_id=message.from_user.id,
-                                 full_name=message.from_user.full_name,
-                                 username=message.from_user.username)
-    except asyncpg.exceptions.UniqueViolationError:
-        user = await db.select_user(telegram_id=message.from_user.id)
-
-    await message.answer("Xush kelibsiz!")
-
-    # ADMINGA xabar beramiz
-    count = await db.count_users()
-    msg = f"{user[1]} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor."
-    await bot.send_message(chat_id=ADMINS[0], text=msg)
+    user_id = message.from_user.id
+    groups = await db.select_all_groups()
+    if groups:
+        for item in groups:
+            group = item["group_id"]
+            if await is_member(bot=bot, group=group, user_id=user_id):
+                await message.answer(f"Assalomu alekum, {message.from_user.full_name}\n\nOldingi guruhlarda qatnashganligiz uchun loyihada qayta qatnasha olmaysiz!\n\n{'-'*10}\n#TODO\nShu yerga mos matn yozish kerak")
+                return
+    await message.answer(
+        text=f"Assalomu aleykum, {message.from_user.full_name}!\n\nHimmat 700+ loyihasiga qatnashish uchun ariza qabul qiluvchi botga xush kelibsiz!\n\nAriza yuborish uchun quyidagi tugmani bosing."
+    )
