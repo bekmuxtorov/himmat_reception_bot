52201a9de83514ae4ef6b96c2fecc5e7ea3a2232
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-07 22:50:38 +0500

    ➕ Added inline buttons

---
+++ keyboards/inline/inline_buttons.py
@@ -0,0 +1,5 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+agree_buttons = InlineKeyboardMarkup(row_width=1)
+agree_buttons.insert(InlineKeyboardButton(text="Ha", callback_data="agree_yes"))
+agree_buttons.insert(InlineKeyboardButton(text="Yo'q", callback_data="agree_no"))
