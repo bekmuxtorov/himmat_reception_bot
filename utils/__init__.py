8997390a00ab9dfdb4d266b670dc5196cb09064c
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-08 14:30:54 +0500

    📝 Added const_text

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from . import db_api
 from . import misc
 from . import define_is_member
+from . import const_texts
 from .notify_admins import on_startup_notify
---
+++ utils/const_texts.py
@@ -0,0 +1,2 @@
+submit_application = "📝 Ariza yuborish"
+terms = "Qatnashish uchun bizni shartlarimiz:\n\n1-shart: ...\n2-shart ...\n3-shart ..."
