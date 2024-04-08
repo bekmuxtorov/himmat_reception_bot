511101ff8209059e6ecc5008f3eaf7b6c9187728
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-08 15:29:55 +0500

    📝 Written set_message function

---
+++ utils/set_message.py
@@ -0,0 +1,12 @@
+async def set_message(user_data: dict, to_admin: bool) -> str:
+    gender = user_data.get("gender")
+    full_name = user_data.get("full_name")
+    text = ""
+    if to_admin:
+        text += "💡 Yangi ariza jo'natildi.\n\n"
+    else:
+        text += "💡 Yuborilayotgan ma'lumotlar.\n\n"
+        
+    text += f"<b>Jins</b>: {gender}\n"
+    text += f"<b>Ism va familiya:</b> {full_name}"
+    return text
