262f733c31c421046450e150c4b5d0e891c1c1dc
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-09 06:29:49 +0500

    📝 Updated text

--- utils/const_texts.py
+++ utils/const_texts.py
@@ -1,3 +1,3 @@
 submit_application = "📝 Ariza yuborish"
 terms = "Qatnashish uchun bizni shartlarimiz:\n\n1-shart: ...\n2-shart ...\n3-shart ..."
-send_message_to_admin = "📝 Adminga xabar yuborish"
+send_message_to_admin_text = "📝 Adminga xabar yuborish"
