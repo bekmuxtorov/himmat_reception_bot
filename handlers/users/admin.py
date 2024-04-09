1412bb70e4976a3179d7f4b095c2d24e73e948fe
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-09 06:30:52 +0500

    ➕ Added admin commands

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -1,15 +1,86 @@
 import asyncio
+import pytz
 
 from aiogram import types
+from datetime import datetime
 
 from data.config import ADMINS
+from filters.is_group import IsGroup, IsGroupAdmin
 from loader import dp, db, bot
 
-@dp.message_handler(text="/reklama", user_id=ADMINS)
+
+@dp.message_handler(IsGroupAdmin(), IsGroup(), commands="group_for_users")
+async def add_group(message: types.Message):
+    group_data = message.chat
+    user_data = message.from_user
+    now_date = pytz.timezone("Asia/Tashkent").localize(datetime.now())
+    data = await db.select_group(group_id=group_data.id)
+    if data:
+        service_message = await message.answer(f"ℹ️ {data.get('group_name')} guruhi bazaga qo'shilgan.")
+        await asyncio.sleep(12)
+        await message.delete()
+        await service_message.delete()
+        return
+
+    now_date = pytz.timezone("Asia/Tashkent").localize(datetime.now())
+    await db.add_group(
+        by_user_id=user_data.id,
+        by_user_name=user_data.full_name,
+        group_id=group_data.id,
+        group_name=group_data.full_name,
+        created_at=now_date,
+        for_whom="for_users"
+    )
+    service_message = await message.answer(
+        text=f"✅ Guruh muaffaqiyatli qo'shildi!"
+    )
+    await asyncio.sleep(12)
+    await message.delete()
+    await service_message.delete()
+
+
+@dp.message_handler(IsGroupAdmin(), IsGroup(), commands=['group_for_man_admin', 'group_for_woman_admin'])
 async def send_ad_to_all(message: types.Message):
-    users = await db.select_all_users()
-    for user in users:
-        # print(user[3])
-        user_id = user[3]
-        await bot.send_message(chat_id=user_id, text="@SariqDev kanaliga obuna bo'ling!")
-        await asyncio.sleep(0.05)
+    command = message.get_command()
+    command_items = command.split('_')
+    for_whom = command_items[2] + "_" + command_items[3]
+    gender = "Erkak" if command_items[2] == "man" else "Ayol"
+    group_data = message.chat
+    user_data = message.from_user
+    data = await db.select_group(group_id=group_data.id)
+    if data:
+        for_whom = data.get("for_whom")
+        if for_whom == "man_admin":
+            service_message = await message.answer(f"ℹ️ <b>{group_data.full_name}</b> guruhi erkaklar uchun admin guruhi sifatida qo'shilgan.")
+        else:
+            service_message = await message.answer(f"ℹ️ <b>{group_data.full_name}</b> guruhi ayollar uchun admin guruhi sifatida qo'shilgan.")
+        await asyncio.sleep(12)
+        await message.delete()
+        await service_message.delete()
+        return
+
+    await db.delete_group(for_whom=for_whom)
+
+    now_date = pytz.timezone("Asia/Tashkent").localize(datetime.now())
+    await db.add_group(
+        by_user_id=user_data.id,
+        by_user_name=user_data.full_name,
+        group_id=group_data.id,
+        group_name=group_data.full_name,
+        created_at=now_date,
+        for_whom=for_whom
+    )
+    service_message = await message.answer(
+        text=f"✅ {gender}lar uchun admin guruh muaffaqiyatli qo'shildi!"
+    )
+    await asyncio.sleep(12)
+    await message.delete()
+    await service_message.delete()
+
+
+@dp.message_handler(IsGroup(), commands=['group_for_man_admin', 'group_for_woman_admin', 'group_for_users'])
+async def send_ad_to_all(message: types.Message):
+    service_message = await message.answer("⚡ Ushbu buyruqni faqat guruh adminlari ishlata oladi.")
+    await asyncio.sleep(12)
+    await message.delete()
+    await service_message.delete()
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,6 +5,11 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
             types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("help", "Yordam"),
+            types.BotCommand("group_for_users",
+                             "Foydalanuvchilar uchun guruh qo'shish"),
+            types.BotCommand("group_for_man_admin",
+                             "Erkak adminlar uchun guruh qo'shish"),
+            types.BotCommand("group_for_woman_admin",
+                             "Ayol adminlar uchun guruh qo'shish"),
         ]
     )
