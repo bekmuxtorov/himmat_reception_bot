183ada26881155daf5065a4308eaf31566f2c486
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-09 06:31:09 +0500

    ➕ Added filter

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,10 +1,12 @@
 from aiogram import Dispatcher
 
 from loader import dp
-from .is_group import IsGroup
+from .is_group import IsGroup, IsGroupAdmin
 from .is_private import IsPrivate
 
 
 if __name__ == "filters":
     dp.filters_factory.bind(IsGroup)
     dp.filters_factory.bind(IsPrivate)
+    dp.filters_factory.bind(IsGroupAdmin)
+
--- filters/is_group.py
+++ filters/is_group.py
@@ -5,3 +5,9 @@ from aiogram import types
 class IsGroup(BoundFilter):
     async def check(self, message: types.Message) -> bool:
         return message.chat.type in (types.ChatType.GROUP, types.ChatType.SUPERGROUP)
+
+
+class IsGroupAdmin(BoundFilter):
+    async def check(self, message: types.Message) -> bool:
+        member = await message.chat.get_member(message.from_user.id)
+        return member.is_chat_admin()
