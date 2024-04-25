57663e29a177f8465b2adf8670909757a9bdc326
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-26 00:00:26 +0500

    📝 Updated set_message function

--- utils/set_message.py
+++ utils/set_message.py
@@ -1,12 +1,14 @@
 async def set_message(user_data: dict, to_admin: bool) -> str:
     gender = user_data.get("gender")
     full_name = user_data.get("full_name")
+    curse_name = user_data.get("course_name")
     text = ""
     if to_admin:
         text += "💡 Yangi ariza jo'natildi.\n\n"
     else:
         text += "💡 Yuborilayotgan ma'lumotlar.\n\n"
-        
+
     text += f"<b>Jins</b>: {gender}\n"
+    text += f"<b>Kurs nomi</b>: {curse_name}\n"
     text += f"<b>Ism va familiya:</b> {full_name}"
     return text
