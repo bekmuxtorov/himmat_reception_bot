0c0d846f0058a8f3fd2f95d2a7b2623189b52e5b
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-04-26 00:00:50 +0500

    ⚡ Deleted commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,14 +4,6 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands(
         [
-            types.BotCommand("start", "Botni ishga tushurish"),
-            types.BotCommand("group_for_man_users",
-                             "Erkak foydalanuvchilar uchun guruh qo'shish"),
-            types.BotCommand("group_for_woman_users",
-                             "Ayol foydalanuvchilar uchun guruh qo'shish"),
-            types.BotCommand("group_for_man_admin",
-                             "Erkak adminlar uchun guruh qo'shish"),
-            types.BotCommand("group_for_woman_admin",
-                             "Ayol adminlar uchun guruh qo'shish"),
+            types.BotCommand("start", "Botni ishga tushurish"), 
         ]
     )
