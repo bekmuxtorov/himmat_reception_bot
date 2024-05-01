cf86e6c9de699c25a06625de9985dc4511d6774d
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-05-01 23:06:25 +0500

    ➕ Added some topics

--- handlers/groups/send_message_to_admin.py
+++ handlers/groups/send_message_to_admin.py
@@ -10,25 +10,25 @@ async def create_topic(message: types.Message):
     await message.answer("Topic'larni yaratish boshlandi.")
     for group in (FOR_MAN_ADMINS, FOR_WOMAN_ADMINS):
         # for accepted questions
-        if not await db.select_topic(topic_created_group_id=group, for_purpose="accepted_questions"):
-            data = await bot.create_forum_topic(chat_id=group, name="Kelib tushgan savollar")
+        if not await db.select_topic(topic_created_group_id=group, for_purpose="accepted_messages"):
+            data = await bot.create_forum_topic(chat_id=group, name="Kelib tushgan xabarlar")
             await db.add_topic(
                 topic_id=data.message_thread_id,
                 topic_name=data.name,
                 topic_created_group_id=group,
                 created_at=await get_now(),
-                for_purpose="accepted_questions"
+                for_purpose="accepted_messages"
             )
 
         # for answered questions
-        if not await db.select_topic(topic_created_group_id=group, for_purpose="answered_questions"):
-            data = await bot.create_forum_topic(chat_id=group, name="Javob berilgan savollar")
+        if not await db.select_topic(topic_created_group_id=group, for_purpose="send_message"):
+            data = await bot.create_forum_topic(chat_id=group, name="Userga yuborilgan xabarlar")
             await db.add_topic(
                 topic_id=data.message_thread_id,
                 topic_name=data.name,
                 topic_created_group_id=group,
                 created_at=await get_now(),
-                for_purpose="answered_questions"
+                for_purpose="send_message"
             )
 
         # for created courses
