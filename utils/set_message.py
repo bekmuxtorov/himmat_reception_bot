0a60c43671bb688d9ab6cc051d8b6298bc7ec3e5
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-05-17 10:17:52 +0500

    Added space

--- utils/set_message.py
+++ utils/set_message.py
@@ -12,7 +12,7 @@ async def set_message(user_data: dict, to_admin: bool) -> str:
 
     text += f"<b>Jins</b>: {gender}\n"
     # text += f"<b>Kurs nomi</b>: {curse_name}\n"
-    text += f"<b>Ism va familiya:</b> {full_name}"
-    text += f"<b>Telegram nomi:</b> {telegram_name}"
+    text += f"<b>Ism va familiya:</b> {full_name}\n"
+    text += f"<b>Telegram nomi:</b> {telegram_name}\n"
     text += f"<b>Username</b> {username}"
     return text
